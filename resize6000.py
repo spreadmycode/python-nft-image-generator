b66c41d7ecae077ba4f1dc8d36e9ac70e8cc975e
spreadmycode <91585048+spreadmycode@users.noreply.github.com>
2022-03-10 14:17:59 -0800

    garbage clean

--- resize6000.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image 
-
-for i in range(4000, 6000):
-    print(i)
-    image = Image.open('./images/' + str(i) + '.png').convert("RGBA")
-    image = image.resize((1024, 1024), Image.ANTIALIAS)
-    image.save('./images/' + str(i) + '.png')
